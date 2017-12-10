8cf6cd70873aa09f6c1e9d3cbd89099ba4630186
hanshufang <hanshufang@mogoroom.com>
2017-12-10 15:43:00 +0800

    opt@new_20171209new_20171209#:购物车底部数据交互完成

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,16 +3,17 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'hightlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'hightlight':totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">0元</div>
+        <div class="price" :class="{'hightlight':totalPrice>0}">￥{{totalPrice}}</div>
         <div class="desc">另需配送费{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥{{minPrice}}元起送
+        <div class="pay" :class="payClass">
+          {{payDesc}}
         </div>
       </div>
     </div>
@@ -21,6 +22,12 @@
 <script>
   export default {
     props: {
+      selectFoods: {
+        type: Array,
+        default () {
+          return [];
+        }
+      },
       deliveryPrice: {
         type: Number,
         default: 0
@@ -32,8 +39,41 @@
     },
     data () {
       return {}
+    },
+    computed: {
+      totalPrice () {
+        let total = 0;
+        this.selectFoods.forEach((item) => {
+          total += item.price * item.count;
+        });
+        return total;
+      },
+      totalCount () {
+        let count = 0;
+        this.selectFoods.forEach((item) => {
+          count += item.count;
+        });
+        return count;
+      },
+      payDesc () {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`;
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return `还差￥${diff}元`;
+        } else {
+          return '去结算';
+        }
+      },
+      payClass () {
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough';
+        } else {
+          return 'enough';
+        }
+      }
     }
-  }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
@@ -69,10 +109,28 @@
             border-radius: 50%
             background: #2b343c
             text-align: center
+            &.hightlight
+              background: rgb(0, 160, 220)
             .icon-shopping_cart
               font-size: 24px
               color: #80858a
               line-height: 44px
+              &.hightlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            color: #fff
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            background: rgb(240, 20, 20)
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
         .price
           display: inline-block
           font-size: 16px
@@ -84,6 +142,8 @@
           padding-right: 12px
           border-right: 1px solid rgba(255, 255, 255, 0.1)
           color: rgba(255, 255, 255, 0.4)
+          &.hightlight
+            color: #fff
         .desc
           display: inline-block
           vertical-align: top
@@ -94,12 +154,15 @@
         flex: 0 0 105px
         width: 105px
         .pay
-          height:48px
-          line-height:48px
-          text-align :center
-          font-size:12px
-          font-weight:700
-          background:#2b333b
-
-
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          font-weight: 700
+          background: #2b333b
+          &.not-enough
+            background: #2b333b
+          &.enough
+            background: #00b43c
+            color: #fff
 </style>
