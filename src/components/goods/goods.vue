446116b539f10a95bdc4bc148efc1be975f471a6
hanshufang <hanshufang@mogoroom.com>
2017-12-10 11:56:04 +0800

    opt@new_20171209#:商品页基础布局，引入better-scoll插件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,15 +1,145 @@
 <template>
-  <div>goods</div>
+  <div class="goods">
+    <div class="menu-wrapper" ref="menuWrapper">
+      <ul>
+        <li class="menu-item" v-for="item in goodData">
+          <span class="menu-text border-1px">{{item.name}}</span>
+        </li>
+      </ul>
+    </div>
+    <div class="food-wrapper" ref="foodWrapper">
+      <ul>
+        <li v-for="item in goodData" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon"/>
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now-price">￥{{food.price}}</span><span class="old-price" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
+  import BScroll from 'better-scroll';
+
   export default {
     name: '',
     data () {
       return {
-        msg: ''
+        goodData: []
       };
+    },
+    mounted () {
+      this.$ajax.get('/api/goods.json')
+        .then((res) => {
+          this.goodData = res.data.content;
+          this.$nextTick(() => {
+            this.initScroll();
+          });
+        });
+    },
+    methods: {
+      initScroll () {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {}); // new BScroll()第一个参数是个dom,第二个参数是option
+        this.foodScroll = new BScroll(this.$refs.foodWrapper, {});
+      }
     }
   };
 </script>
 
-<style scoped></style>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "../../common/stylus/mixin"
+
+  .goods
+    width: 100%;
+    display: flex
+    overflow: hidden
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 46px;
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        line-height: 14px
+        width: 56px
+        padding: 0 12px
+        .menu-text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          font-size: 12px
+          border-1px(rgba(7, 1, 27, 0.1))
+    .food-wrapper
+      flex: 1;
+      background: #fff
+      .food-list
+        .title
+          padding-left: 14px
+          height: 26px
+          line-height: 26px
+          border-left: 2px solid #d9dde1
+          font-size: 12px
+          color: rgb(147, 153, 159)
+          background: #f3f5f7
+        .food-item
+          display: flex
+          margin: 18px
+          padding-bottom: 18px
+          border-1px(rgba(7, 1, 27, 0.1))
+          &:last-child
+            border-none()
+            margin-bottom: 0
+          .icon
+            flex: 0 0 57px
+            margin-right: 10px
+          .content
+            flex: 1
+            .name
+              font-size: 14px
+              height: 14px
+              line-height: 14px
+              margin: 2px 0 8px 0
+            .desc, .extra
+              font-size: 10px
+              color: rgb(147, 153, 159)
+            .desc
+              line-height: 12px
+              margin-bottom: 8px
+            .extra
+              line-height: 10px
+              .count
+                margin-right: 12px
+            .price
+              font-weight: 700
+              line-heigt: 24px
+              .now-price
+                margin-right: 8px
+                font-size: 14px
+                color: rgb(240, 20, 20)
+              .old-price
+                text-decoration: line-through
+                font-size: 10px
+                color: rgb(147, 153, 159)
+
+
+</style>
+
